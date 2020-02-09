bfb54f341e0a9babb4b8baa51b8d962429926500
EsauCS <yo.esau@gmail.com>
2020-02-09 00:48:54 -0600

    Conexion a base en linea
    Co-Authored-By: null <EsauCS@users.noreply.github.com>

--- conexion.php
+++ conexion.php
@@ -2,10 +2,10 @@
 
 function conectar()
 {
-  $user="root";
-  $pass="";
-  $server="localhost";
-  $db="login";
+  $user="K95XYrvS2W";
+  $pass="iP5BEPlGCc";
+  $server="remotemysql.com";
+  $db="K95XYrvS2W";
   $con=mysqli_connect($server, $user, $pass, $db) or die ("Error al conectar con la base".mysql_error());
   return $con;
 }
